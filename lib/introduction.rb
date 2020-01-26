8baaafed8eeeb5992498bb9744e1970ad70b61e7
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-26 07:16:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 #introduction_with_language 
  
 def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction("Dan", "Ember.js")
